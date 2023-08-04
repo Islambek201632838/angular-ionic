40cfad4e51768b08ff454501c9ce9be6c349746f
Islambek201632838 <islambek.sadykov@nu.edu.kz>
2023-08-05 00:53:21 +0600

    Add project files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: 'main', loadChildren: () => import('./pages/main/main.module').then(m => m.MainPageModule) },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
   { path: '**', redirectTo: 'authentication' },
+  { path: 'second-page', loadChildren: () => import('./pages/second/second.page.modules').then(m => m.SecondPageModule)},
   {
     path: 'authentication',
     loadChildren: () => import('./authentication/authentication.module').then( m => m.AuthenticationPageModule)
